21bdfce63dd81d9c4d76515d1013744c30709b7d
Sukhpreet Singh <ss0218308@gmail.com>
2026-03-03 12:39:50 +0530

    Initial Commit

---
+++ MoodTracker/MoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerApp.swift
+//  MoodTracker
+//
+//  Created by Sukhpreet Singh on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
